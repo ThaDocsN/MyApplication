d5da94fd4eafb962ec3bc3ea4cdcf2f5319582fe
ThaDocsN <charles.tha.doc@gamil.com>
2019-02-05 14:20:49 -0600

    created plant repo

---
+++ app/src/main/java/com/thadocizn/myapplication/PlantRepository.java
@@ -0,0 +1,44 @@
+package com.thadocizn.myapplication;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import com.thadocizn.myapplication.classes.Plant;
+
+import java.util.List;
+
+public class PlantRepository {
+
+    private PlantDao plantDao;
+    private LiveData<List<Plant>> allPlants;
+
+    public PlantRepository(Application application) {
+        PlantRoomDatabase database = PlantRoomDatabase.getDatabase(application);
+        plantDao = database.plantDao();
+        allPlants = plantDao.getAllPlants();
+    }
+
+    LiveData<List<Plant>> getAllPlants(){
+        return allPlants;
+    }
+
+    public void insert(Plant plant){
+        new insertAsyncTask(plantDao).execute(plant);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<Plant, Void, Void> {
+
+        private PlantDao asyncTaskDao;
+
+        public insertAsyncTask(PlantDao plantDao) {
+            asyncTaskDao = plantDao;
+        }
+
+        @Override
+        protected Void doInBackground(Plant... plants) {
+            asyncTaskDao.insert(plants[0]);
+            return null;
+        }
+    }
+}
